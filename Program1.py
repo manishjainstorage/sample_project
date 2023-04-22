e38acd9e4ed10907a4a2a00a229e29e0681b812b
Dr.Manish Jain <33494092+manishjainstorage@users.noreply.github.com>
2023-04-22 08:39:49 +0530

    Add files via upload

---
+++ Program1.py
@@ -0,0 +1,17 @@
+prime_numbers = [11, 3, 7, 5, 2]
+
+# sorting the list in ascending order
+
+prime_numbers.sort()
+
+print(prime_numbers)
+
+
+# vowels list
+vowels = ['e', 'a', 'u', 'o', 'i']
+
+# sort the vowels
+vowels.sort(reverse=True)
+
+# print vowels
+print('Sorted list (in Descending):', vowels)
